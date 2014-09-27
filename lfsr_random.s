83d36a85e565da81018c611fc506ce26a0fd1b03
Matthew Dabit <mattdabit@berkeley.edu>
2014-09-27 14:31:44 -0700

    masked  ad

--- lfsr_random.s
+++ lfsr_random.s
@@ -21,19 +21,20 @@ lfsr_random:
 for:
 	beq $t2 $t1 end #i == 16 return reg 
 	srl $t4 $t3 2  #$t4 = reg >> 2 $t4 is highest
-	xor $t4 $t4 $t3 # t4 = t3 xor t4
-	srl $t5 $t3 3 #y5 = t3 >> 3
-	xor $t4 $t4 $t5 # t4 xor t5
-	srl $t5 $t3 5 # t5 = t3 >> 5
-	xor $t4 $t4 $t5 # t4 - t4 xor t5 
-	srl $t3 $t3 1 # t3 = t3 >> 1
-	sll $t4 $t4 15 # t4 = t4 << 15
-	or $t3 $t3 $t4
-	addiu $t2 $t2 1
-	j for	
+	xor $t4 $t3 $t4 # t4 = (reg>>0) ^ (reg>>2)
+	srl $t5 $t3 3 #t5 = (reg >> 3)
+	xor $t4 $t4 $t5 # t4 = (reg>>0) ^ (reg>>2) xor (reg >> 3)
+	srl $t5 $t3 5 # t5 = reg >> 5
+	xor $t4 $t4 $t5 # t4 = (reg>>0) ^ (reg>>2) xor (reg >> 3) xor (reg >> 5 )
+	srl $t3 $t3 1 # t3 = reg >> 1
+	sll $t4 $t4 15 # t4 = highest << 15
+	andi $t4 $t4 0xFFFF # mask t4
+	or $t3 $t3 $t4 # t3 = reg >> 1 | highest << 15
+	addiu $t2 $t2 1 # i++
+	andi $t3 $t3 0xFFFF #mask t5
+	j for	# redo for loop
 end:	
-	addu $t2 $0 $0	
-	or $v0 $v0 $t3 
+	addu $v0 $0 $t3 
         la $t0 lfsr
         sh $v0 0($t0)
         jr $ra
